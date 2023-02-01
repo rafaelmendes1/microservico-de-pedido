e4a0dd9547d8b4f2908003c4699e6d266f09aa1e
rafaelmendes1 <rarafafa3245@gmail.com>
2023-01-31 22:44:13 -0300

    Adiciona entidades

---
+++ src/main/java/com/rafaelmendes1/pedidos/model/Status.java
@@ -0,0 +1,13 @@
+package com.rafaelmendes1.pedidos.model;
+
+public enum Status {
+
+    REALIZADO,
+    CANCELADO,
+    PAGO,
+    NAO_AUTORIZADO,
+    CONFIRMADO,
+    PRONTO,
+    SAIU_PARA_ENTREGA,
+    ENTREGUE;
+}
